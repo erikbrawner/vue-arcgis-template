c4f00fd8d89467f78b46b447fe9ce66d145a6a94
Erik Brawner <erikbrawner@evbrawner.local>
2022-02-15 16:44:48 -0600

    init commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <ArcGISMap msg="ArcGIS Map Template"/>
+</template>
+
+<script>
+  import ArcGISMap from './components/ArcGISMap.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      ArcGISMap
+    }
+  }
+</script>
+
+<style>
+  @import url('https://js.arcgis.com/4.22/esri/themes/dark/main.css');
+  html, body {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    height: 100%;
+    width: 100%;
+  }
+</style>
---
+++ src/components/ArcGISMap.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class='map-arcgis'></div>
+</template>
+
+<script>
+import Map from '@arcgis/core/Map';
+import MapView from '@arcgis/core/SceneView';
+
+import esriConfig from '@arcgis/core/config.js';
+
+export default {
+  name: 'ArcgisMap',
+  props: {
+    msg: String
+  },
+  mounted() {
+    esriConfig.assetsPath = './assets';
+
+    let date = new Date();
+
+    const map = new Map({
+      map, 
+      container: this.$el,
+      zoom: 16,
+      camera: {
+        position: [-93.616, 41.589, 32000],
+        heading: 0
+      },
+      environment: {
+        starsEnabled: true,
+        atmosphereEnabled: true,
+        lighting: {
+          date: date,
+          directShadowsEnabled: true
+        }
+      }
+    });
+
+    view.when(() => {
+      console.log('view ready');
+    });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.map-arcgis{
+  height: 100%;
+  width: 100%;
+}
+</style>
